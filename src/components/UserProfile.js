724bf26006369c950214c881ff538e3b2afa8021
AJ Magracia <ajmagracia@gmail.com>
2018-10-10 16:45:52 -0700

    functionality updates

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getUserTips } from '../services/TipService.js'
+import { getOrMakeProfileByUserId as getProfile } from '../services/ProfileService.js'
 import Tip from './Tip'
 import AuthService from '../services/AuthService.js'
 
@@ -9,7 +10,8 @@ class userProfile extends Component {
     this.auth = new AuthService()
     this.state = {
       tips: [],
-      userId: 0
+      userId: 0,
+      profile: {}
     }
   }
 
@@ -18,21 +20,28 @@ class userProfile extends Component {
     let userId
     if(this.auth.loggedIn())
       userId = this.auth.getUserId()
-    getUserTips(this.props.match.params.userId)
-    .then(res=> {
-      console.log(res)
-      this.setState({ tips: res, userId})
+    getProfile(this.props.match.params.userId)
+    .then(profile=> {
+      getUserTips(this.props.match.params.userId)
+      .then(res=> {
+        console.log(res)
+        this.setState({ tips: res, userId, profile})
+      })
     })
   }
 
   render() {
-    let { tips, userId } = this.state
+    let { tips, userId, profile } = this.state
     let tipList = tips.map(tip => {
       return <Tip key={tip.id} id={tip.id} userId={userId}/>
     })
     return (
       <div>
-        <h1>Profile</h1>
+        <h1 children={`${profile.username}'s Profile`} />
+        <img src={profile.image} />
+        <h2 children={`About ${profile.username}`} />
+        <p children={profile.bio} />
+        <h1 children={`How ${profile.username} eats better:`} />
         {tipList}
       </div>
     );
